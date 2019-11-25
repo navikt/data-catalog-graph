7481dbc9cf5fade0c0198fb2980a8ae80023bdfa
Vattekar <42180319+erikvatt@users.noreply.github.com>
2019-11-25 15:22:52 +0100

    use dataverk-vault lib for postgres creds generation and secrets

--- api/database.py
+++ api/database.py
@@ -1,27 +1,33 @@
 import os
+from urllib3.util import parse_url
 import psycopg2
 from dotenv import load_dotenv
 from pathlib import Path
 from psycopg2.extras import RealDictCursor
 import logging
-import secrets_reader
+from dataverk_vault import api as vault_api
+
 
 class Database:
 
     def __init__(self):
         env_path = Path('..') / '.env'
         load_dotenv(dotenv_path=env_path)
-        secrets = secrets_reader.secrets() # os.getenv("DATABASE_URI")
+        secrets = vault_api.read_secrets()
         dsn = secrets["POSTGRES_DB"]
         self.dsn = dsn
+        self.vault_secret_path = os.environ["POSTGRES_VAULT_PATH"]
         self.conn = None
 
     def open_connection(self):
         try:
-            if(self.conn is None):
-                self.conn = psycopg2.connect(self.dsn)
+            self.conn = psycopg2.connect(self.dsn)
         except psycopg2.DatabaseError as e:
             logging.error(e)
+        except psycopg2.OperationalError:
+            self._update_credentials()
+            self.conn = psycopg2.connect(self.dsn)
+            logging.warning(f"Fetching new postgres credentials")
         finally:
             logging.info('Connection opened successfully.')
 
@@ -39,7 +45,6 @@ class Database:
             if self.conn:
                 self.conn.close()
                 logging.info('Database connection closed.')
-  
 
     def execute(self, query):
         try:
@@ -61,4 +66,8 @@ class Database:
             if self.conn:
                 self.conn.close()
                 logging.info('Database connection closed.')
-  
+
+    def _update_credentials(self):
+        credentials = vault_api.get_database_creds(self.vault_secret_path)
+        parsed_conn_string = parse_url(self.dsn)
+        self.dsn = str(parsed_conn_string).replace(parsed_conn_string.auth, credentials)
