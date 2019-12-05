b74b6d2c351048920f69ba745de03d2a54809e5b
JeremiahUy <jery_3000@hotmail.com>
2019-12-05 10:18:41 +0100

    UPDATE create and update when setting values

--- api/node.py
+++ api/node.py
@@ -47,7 +47,7 @@ def create(node):
             abort(409, f"The node must have a prop key with value of type dict")
         if id is None:
             abort(409, f"The prop dict should contain id property of type string")
-        statement = statement + f"({json.dumps(prop_id)}, {json.dumps(prop)}), "
+        statement = statement + f"({json.dumps(prop_id)}, '{json.dumps(prop)}'), "
 
     # insert new
     db = Database()
@@ -69,7 +69,7 @@ def update(node):
         elif id is None:
             abort(409, f"The prop dict should contain id property of type string")
         else:
-            statement = statement + f"({json.dumps(prop_id)}, {json.dumps(prop)}), "
+            statement = statement + f"({json.dumps(prop_id)}, '{json.dumps(prop)}'), "
 
     # insert new
     db = Database()
