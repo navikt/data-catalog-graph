f27951a4644c5f0127aab32c05b278034d7f4ee9
JeremiahUy <jery_3000@hotmail.com>
2019-12-16 14:37:28 +0100

    UPDATE get api endpoint to handle request body

--- api/node.py
+++ api/node.py
@@ -63,10 +63,9 @@ def get_nodes_by_list_of_ids(id_list):
     db = Database()
     statement = "SELECT * FROM tbl_node WHERE"
     for list_item in id_list:
-        node_id = list_item.get('id')
-        if node_id is None:
+        if list_item is None:
             abort(400, f"Request body is missing ID key of type integer")
-        statement = statement + f' id = {node_id} OR'
+        statement = statement + f' id = {list_item} OR'
 
     #DELETE the last OR in statement
     statement = statement[:-2]
