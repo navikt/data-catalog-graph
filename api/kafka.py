126be77c35c6f74ff7748ce62ebdee35f970a3cc
JeremiahUy <jery_3000@hotmail.com>
2020-01-08 11:52:23 +0100

    UNDO changes and update mapping of paramter

--- api/kafka.py
+++ api/kafka.py
@@ -26,23 +26,25 @@ def get_all_valid_kafka_topic_field():
 
 def get_valid_kafka_topic_by_prop_id(prop_id):
     db = Database()
-    statement = f"SELECT * FROM tbl_node WHERE valid = TRUE AND prop->>'type' ILIKE 'kafka_topic' AND prop->>'id' ='{prop_id}'"
+    statement = f"""SELECT * FROM tbl_node WHERE valid = TRUE AND prop->>'type' ILIKE 'kafka_topic' 
+                    AND prop->>'id' = '{prop_id}'"""
     print(statement)
     nodes = db.execute(statement)
 
     if nodes is not None:
         return nodes, 200
 
-    abort(404, f"Kafka topic with prop.id {id} not found")
+    abort(404, f"Kafka topic with prop.id {prop_id} not found")
 
 
 def get_valid_kafka_topic_field_by_prop_id(prop_id):
     db = Database()
-    statement = f"SELECT * FROM tbl_node WHERE valid = TRUE AND prop->>'type' ILIKE 'kafka_topic_field' AND prop->>'id' ='{prop_id}'"
+    statement = f"""SELECT * FROM tbl_node WHERE valid = TRUE AND prop->>'type' ILIKE 'kafka_topic_field' 
+                    AND prop->>'id' = '{prop_id}'"""
     print(statement)
     nodes = db.execute(statement)
 
     if nodes is not None:
         return nodes, 200
 
-    abort(404, f"Kafka topic field with prop.id {id} not found")
+    abort(404, f"Kafka topic field with prop.id {prop_id} not found")
