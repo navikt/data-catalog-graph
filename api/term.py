1ff05517b9f826e04df7eab6ca73e27da01ce10a
JeremiahUy <jery_3000@hotmail.com>
2020-01-17 14:34:42 +0100

    UPDATE: return object for search term and get term by prop_id

--- api/term.py
+++ api/term.py
@@ -19,9 +19,16 @@ def get_valid_node_by_prop_id(id):
     db = Database()
     statement = f"SELECT * FROM tbl_node WHERE valid = TRUE AND prop->>'type' ILIKE 'term' AND prop->>'id' = '{id}'"
     print(statement)
-    node = db.execute(statement)
-    if node is not None:
-        return node, 200
+    nodes = db.execute(statement)
+    pattern = '\[([^|]+)\|([A-Z]{1,10}-\d+)\]'
+    if nodes is not None:
+        for term in nodes:
+            description = re.sub(pattern, r'\1', term['prop']['definisjon'])
+            term.update({'term': term['prop']['term']})
+            term.update({'description': description})
+            term.update({'status': term['prop']['status']})
+
+        return nodes, 200
 
     abort(404, f"Node with prop.id {id} not found")
 
@@ -35,17 +42,12 @@ def search_term_by_name(term_name, term_status='godkjent'):
     nodes = db.execute(statement)
     pattern = '\[([^|]+)\|([A-Z]{1,10}-\d+)\]'
     if nodes is not None:
-        term_list = []
         for term in nodes:
             description = re.sub(pattern, r'\1', term['prop']['definisjon'])
-            term_list.append({
-                'id': term['prop']['id'],
-                'term': term['prop']['term'],
-                'description': description,
-                'status': term['prop']['status'],
-                'prop': term['prop']
-            })
+            term.update({'term': term['prop']['term']})
+            term.update({'description': description})
+            term.update({'status': term['prop']['status']})
 
-        return term_list, 200
+        return nodes, 200
 
     abort(404, f"No match found with term {term_name}")
