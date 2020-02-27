f4f9adc9903f76f34fc3b799d2bfe436e15d32f9
JeremiahUy <jery_3000@hotmail.com>
2020-02-27 10:17:07 +0100

    ADD: logging for debugging

--- api/node.py
+++ api/node.py
@@ -196,7 +196,7 @@ def add_node_comment(node):
 
     #check if node has comments.
     check_statement = f"SELECT prop->>'comments' AS comments FROM tbl_node WHERE id = {node_id}"
-    check_result = db.execute(check_statement)@
+    check_result = db.execute(check_statement)
     logging.warning(check_result)
     #creates comment key if it does not exist.
     if check_result is None:
