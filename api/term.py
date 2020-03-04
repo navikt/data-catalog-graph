f99fce7e7edd21290c0a8ada50915907a1a138e4
JeremiahUy <jery_3000@hotmail.com>
2020-03-04 10:44:03 +0100

    UPDATE: logic and naming of filtered description

--- api/term.py
+++ api/term.py
@@ -20,17 +20,17 @@ def get_valid_node_by_prop_id(id):
     statement = f"SELECT * FROM tbl_node WHERE valid = TRUE AND prop->>'type' ILIKE 'term' AND prop->>'id' = '{id}'"
 
     nodes = db.execute(statement)
-    pattern_with_site_link = """\[([^|]+)\|((www.jira|http:\/\/jira|https:\/\/jira|http:\/\/www.jira|https:\/\/www.jira)
-                                +[^\s]+[\w])\]"""
+    pattern_with_site_link = '\[([^|]+)\|((www.jira|http:\/\/jira|https:\/\/jira|http:\/\/www.jira|https:\/\/www' \
+                             '.jira)+[^\s]+[\w])\]'
     pattern = '\[([^|]+)\|([A-Z]{1,10}-\d+)\]'
     if nodes is not None:
         for term in nodes:
-            description = re.sub(pattern, r'\1', term['prop']['definisjon'])
-            description = re.sub(pattern_with_site_link, r'\1', description)
+            filtered_description = re.sub(pattern, r'\1', term['prop']['definisjon'])
+            description_without_site_links = re.sub(pattern_with_site_link, r'\1', filtered_description)
             term.update({'node_id': term['id']})
             term.update({'id': term['prop']['id']})
             term.update({'term': term['prop']['term']})
-            term.update({'description': description})
+            term.update({'description': description_without_site_links})
             term.update({'status': term['prop']['status']})
 
         return nodes, 200
@@ -45,17 +45,17 @@ def search_term_by_name(term_name, term_status='godkjent'):
                   (prop->>'term' ILIKE '%{term_name}%' OR prop->>'definisjon' ILIKE '%{term_name}%') AND 
                   prop->>'status' ILIKE '%{term_status}%' """
     nodes = db.execute(statement)
-    pattern_with_site_link = """\[([^|]+)\|((www.jira|http:\/\/jira|https:\/\/jira|http:\/\/www.jira|https:\/\/www.jira)
-                                +[^\s]+[\w])\]"""
+    pattern_with_site_link = '\[([^|]+)\|((www.jira|http:\/\/jira|https:\/\/jira|http:\/\/www.jira|https:\/\/www' \
+                             '.jira)+[^\s]+[\w])\]'
     pattern = '\[([^|]+)\|([A-Z]{1,10}-\d+)\]'
     if nodes is not None:
         for term in nodes:
-            description = re.sub(pattern, r'\1', term['prop']['definisjon'])
-            description = re.sub(pattern_with_site_link, r'\1', description)
+            filtered_description = re.sub(pattern, r'\1', term['prop']['definisjon'])
+            description_without_site_links = re.sub(pattern_with_site_link, r'\1', filtered_description)
             term.update({'node_id': term['id']})
             term.update({'id': term['prop']['id']})
             term.update({'term': term['prop']['term']})
-            term.update({'description': description})
+            term.update({'description': description_without_site_links})
             term.update({'status': term['prop']['status']})
 
         return nodes, 200
