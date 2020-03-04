80b72e248be66fdc6cb40a86bf9aad5bb69ffa7d
JeremiahUy <jery_3000@hotmail.com>
2020-03-04 10:23:10 +0100

    UPDATE: logic for filtering jira text for site links

--- api/term.py
+++ api/term.py
@@ -20,10 +20,13 @@ def get_valid_node_by_prop_id(id):
     statement = f"SELECT * FROM tbl_node WHERE valid = TRUE AND prop->>'type' ILIKE 'term' AND prop->>'id' = '{id}'"
 
     nodes = db.execute(statement)
+    pattern_with_site_link = """\[([^|]+)\|((www.jira|http:\/\/jira|https:\/\/jira|http:\/\/www.jira|https:\/\/www.jira)
+                                +[^\s]+[\w])\]"""
     pattern = '\[([^|]+)\|([A-Z]{1,10}-\d+)\]'
     if nodes is not None:
         for term in nodes:
             description = re.sub(pattern, r'\1', term['prop']['definisjon'])
+            description = re.sub(pattern_with_site_link, r'\1', description)
             term.update({'node_id': term['id']})
             term.update({'id': term['prop']['id']})
             term.update({'term': term['prop']['term']})
@@ -42,10 +45,13 @@ def search_term_by_name(term_name, term_status='godkjent'):
                   (prop->>'term' ILIKE '%{term_name}%' OR prop->>'definisjon' ILIKE '%{term_name}%') AND 
                   prop->>'status' ILIKE '%{term_status}%' """
     nodes = db.execute(statement)
+    pattern_with_site_link = """\[([^|]+)\|((www.jira|http:\/\/jira|https:\/\/jira|http:\/\/www.jira|https:\/\/www.jira)
+                                +[^\s]+[\w])\]"""
     pattern = '\[([^|]+)\|([A-Z]{1,10}-\d+)\]'
     if nodes is not None:
         for term in nodes:
             description = re.sub(pattern, r'\1', term['prop']['definisjon'])
+            description = re.sub(pattern_with_site_link, r'\1', description)
             term.update({'node_id': term['id']})
             term.update({'id': term['prop']['id']})
             term.update({'term': term['prop']['term']})
